03b3f2789a2479a2dbfda1e66aac2f795db79047
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-01-11 16:12:32 +0200

    DayChallenge

---
+++ Week3/DayChallengeW3_Day2/index.js
@@ -0,0 +1,25 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+console.log(fruits);
+
+let fruits = ['Apples', 'Oranges', 'Blueberries'];
+document.getElementById("list").innerHTML = fruits.sort();
+ ['Apples', 'Blueberries', 'Oranges']
+
+let fruits = fruits.push('Kiwi');
+console.log(fruits);
+
+fruits.splice(0,1);
+console.log(fruits);
+['Apples']
+
+fruits.reverse();
+console.log();
+(3) ['Kiwi', 'Blueberries', 'Oranges']
+
+// second exercise
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[0]);
+//I can't make ir dissplay. I ran out of solutions. Can't really understand what I'm doing wrong.
