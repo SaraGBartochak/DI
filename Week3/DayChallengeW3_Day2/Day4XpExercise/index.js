38cc061d5b5de22ce7e2e6f231c8521589e71a68
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-01-12 23:03:11 +0200

    XP day4

---
+++ Week3/DayChallengeW3_Day2/Day4XpExercise/index.js
@@ -0,0 +1,55 @@
+//Exercise 1
+
+let x = 7
+let y = 3
+
+let answer = prompt("Who is bigger? x=7 or y=3?")
+
+if ( x > y) {
+    alert("x is the biggest number");
+} else {
+    alert("Sorry");
+}
+
+//Exercise 2: Chihuahua
+
+let newDog = 'Chihuahua';
+console.log(length);
+
+newDog.toUpperCase();
+console.log();
+
+newDog.toLowerCase();
+console.log();
+
+if (newDog === "Chihuahua") {
+    prompt("I love Chihuahuas, it's my favorite dog breed");
+} else {
+    prompt("I don't care, I prefer cats")
+}
+
+//Exercise 3
+
+let user = prompt("Type any number:");
+
+document.getElementsByName(user).value;
+
+if (user % 2 == 0) {
+    prompt('is an even number');
+} else { 
+    prompt('is a odd number');
+}
+
+//Exercise 4
+
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+console.log();
+
+switch (users) {
+    case []:
+    console.log("no one is online");
+    break;
+
+case [1]:
+    console.log()
+} 
