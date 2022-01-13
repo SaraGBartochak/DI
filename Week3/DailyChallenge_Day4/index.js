166239f0f57a4b8598bd1e19e163b26cabfaa191
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-01-13 16:24:59 +0200

    Daily Challange done, and update folder

---
+++ Week3/DailyChallenge_Day4/index.js
@@ -0,0 +1,12 @@
+let sentence = "not bad";
+
+let wordNot = "not bad";
+
+let wordBad = "bad not";
+
+if (sentence === wordNot) {
+       console.log("This movie is good, I like it");
+} else {
+    console.log("The movie is not that bad, I like it");
+}
+    
