0cf1aaaabb63b3aa2e348a610568dd0e41f96f2a
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-01-15 16:22:59 +0200

    Daily Challenge Stars

---
+++ Week3/LoopExercises/DailyChallengeStars/index.js
@@ -0,0 +1,14 @@
+let star = ["*", "**", "***", "****", "*****", "******"];
+
+for (let index = 0; index < star.length; index++) {
+    console.log(star[index]);
+}
+
+for (let index = 0; index < star.length; index++) {
+    console.log(star[index]);
+
+    for (let index = 0; index < star.length; index++) {
+        console.log(star[index]);
+        
+    }
+}
