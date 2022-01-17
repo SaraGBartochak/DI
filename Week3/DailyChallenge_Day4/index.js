80f2332d852d6f5120041f40fe096c17ee7f6d22
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-01-17 11:23:41 +0200

    Redo Daily challenge day 4

--- Week3/DailyChallenge_Day4/index.js
+++ Week3/DailyChallenge_Day4/index.js
@@ -1,11 +1,11 @@
-let sentence = "not bad";
+let sentence = "The movie is not that bad, I like it";
 
-let wordNot = "not bad";
+let wordNot = "The movie is not that bad, I like it";
 
-let wordBad = "bad not";
+let wordBad = "The movie is bad, I don't like it";
 
 if (sentence === wordNot) {
-       console.log("This movie is good, I like it");
+       console.log("This movie is good, I like it"); 
 } else {
     console.log("The movie is not that bad, I like it");
 }
