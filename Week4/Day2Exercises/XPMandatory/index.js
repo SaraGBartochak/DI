9160f69d3d353c20538c6315a09bf8bc214ceb93
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-01-18 21:12:03 +0200

    mini project

---
+++ Week4/Day2Exercises/XPMandatory/index.js
@@ -0,0 +1,87 @@
+// // Part 1
+
+// function infoAboutMe() {
+//     name: "Sara";
+//     age: 34;
+//     hobbies: "crafts",
+//     console.log(infoAboutMe);
+// }
+// infoAboutMe();
+
+// // Part 2
+
+// function infoAboutPerson(personName, personAge, personFavoriteColor) {
+//     console.log("Your name is", personName, "you are", personAge, "years old, your favorite color is", personFavoriteColor)
+// }
+
+// infoAboutPerson("David", 45, "blue");
+// infoAboutPerson("Josh", 12, "yellow");
+
+// // Exercise 2: Tips
+
+// function calculateTip() {
+
+// let bill = parseInt(prompt("Enter your bill"));
+// let tip;
+
+//    if (bill < 50) { 
+//        tip = bill * 0.2;
+//        console.log(tip)
+
+//    } else if (bill >= 50 && bill <= 200){ 
+//     tip = bill * 0.15;
+//     console.log(tip)
+
+//    } else if (bill >= 200) {
+//        tip = bill * 0.1;
+//        console.log(tip)
+//    }
+//    console.log(bill + tip);
+// }
+// calculateTip();
+
+// Exercise 3
+
+function isDivisible(divisor) {
+  let sum = 0;
+  let outcome = [];
+
+  for (let index = 0; index <= 500; index++) {
+        
+      if (index % divisor === 0) { 
+        console.log(index);
+        sum += index;
+        console.log(sum);
+  }
+}
+}
+isDivisible(23);
+
+// Exercise 4
+
+let stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
+
+let prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+} 
+
+shoppingList = ["banana", "orange", "apple"];
+
+function myBill() {
+    let total = 0;
+    array.forEach(element => {
+        
+    });
+}
+myBill();
+
---
+++ Week4/MiniProjectDay3/index.js
@@ -0,0 +1,10 @@
+function playTheGame() {
+
+    let userPrompt = parseInt(confirm("Would you like to play a game?"));
+    if (confirm == true) {
+        console.log(confirm);
+    } else if (confirm == false) {
+        alert("No problem, goodbye!");
+    }
+
+}
