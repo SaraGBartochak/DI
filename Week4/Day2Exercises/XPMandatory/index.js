498e16ee65e8078a5957baa59ac772f32bb69cbe
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-01-22 13:23:42 +0200

    update

--- Week4/Day2Exercises/XPMandatory/index.js
+++ Week4/Day2Exercises/XPMandatory/index.js
@@ -6,7 +6,7 @@
 //     hobbies: "crafts",
 //     console.log(infoAboutMe);
 // }
-// infoAboutMe();
+// infoAboutMe(key, value);
 
 // // Part 2
 
@@ -42,46 +42,166 @@
 
 // Exercise 3
 
-function isDivisible(divisor) {
-  let sum = 0;
-  let outcome = [];
-
-  for (let index = 0; index <= 500; index++) {
+// function isDivisible(divisor) {
+//   let sum = 0;
+ 
+//   for (let index = 0; index <= 500; index++) {
         
-      if (index % divisor === 0) { 
-        console.log(index);
-        sum += index;
-        console.log(sum);
-  }
-}
-}
-isDivisible(23);
+//       if (index % divisor === 0) { 
+//         sum += index;
+//         console.log(sum);
+//   }
+// }
+// }
+// isDivisible(23);
 
 // Exercise 4
 
-let stock = { 
-    "banana": 6, 
-    "apple": 0,
-    "pear": 12,
-    "orange": 32,
-    "blueberry":1
-}  
+// let stock = { 
+//     "banana": 6, 
+//     "apple": 0,
+//     "pear": 12,
+//     "orange": 32,
+//     "blueberry":1
+// }  
 
-let prices = {    
-    "banana": 4, 
-    "apple": 2, 
-    "pear": 1,
-    "orange": 1.5,
-    "blueberry":10
-} 
+// let prices = {    
+//     "banana": 4, 
+//     "apple": 2, 
+//     "pear": 1,
+//     "orange": 1.5,
+//     "blueberry":10
+// } 
 
-shoppingList = ["banana", "orange", "apple"];
+// shoppingList = ["banana", "orange", "apple"];
 
-function myBill() {
-    let total = 0;
-    array.forEach(element => {
-        
-    });
+// function myBill() {
+
+//     let total = 0;
+//     for (let i = 0; i < shoppingList.length; i++) {
+
+//         if (stock[shoppingList[i]] > 0) {
+//             stock[shoppingList[i]] --;
+//             total += prices[shoppingList[i]];
+//         }
+//     }
+//     return total;
+// }
+// console.log(myBill());
+
+// // Exercise 5
+
+// function changeEnough(itemPrice, amountOfChange){
+
+//     let money = [0.25, 0.1, 0.05, 0.01];
+//     let sumOfChange = 0;
+
+//     // money.forEach(i => {
+//     //     sumOfChange += amountOfChange[i] * money[i];
+//     // });
+    
+//     for (let i in amountOfChange) {
+//         sumOfChange += amountOfChange[i] * money[i];
+//     }
+//     console.log(sumOfChange);
+
+//     if (sumOfChange >= itemPrice) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(changeEnough(4.25, [25, 20, 5, 0]));
+
+// Exercise 6
+
+function hotelCost(){
+    
+    let perNight = 140;
+    let numberOfNights;
+ 
+    do {
+        numberOfNights = parseInt(prompt("How many nights you would like to purchase?"));
+    } 
+    while (numberOfNights < 0 || Number.isNaN(numberOfNights)) {
+        return numberOfNights * perNight;
+    }
 }
-myBill();
+console.log(hotelCost());
 
+let londonDestination = "London";
+let parisDestination = "Paris";
+let otherDestination = "";
+let londonDestinationCost = 183;
+let parisDestinationCost = 220;
+let otherDestinationsCost = 300;
+
+function planeRideCost(){
+    
+   let destination = "";
+    do {
+        destination = prompt("Please, choose a destination");
+    }
+    while (destination === "" || !/^[a-zA-Z()]*$/.test(destination));
+    return getCostByDestination(destination);
+}
+
+let getCostByDestination = destination => {
+
+    let cost;
+
+    switch(destination){
+
+        case londonDestination: cost = londonDestinationCost;
+
+            break;
+
+        case parisDestination: cost = parisDestinationCost;
+
+            break;
+
+        default: cost = otherDestinationsCost;
+    }
+
+    return cost;
+}
+
+
+planeRideCost();
+
+function rentalCarCost(){
+
+    let carCost = 40;
+    let rentDays;
+    let discount = 0.05;
+
+    do { 
+        rentDays = parseInt(prompt("How many days would you like to rent a car?"));
+    }
+    while (rentDays === true)
+    if (rentDays === "" || !isNaN(rentDays)) {
+
+    } else if (rentDays <= 10) {
+        return carCost * rentDays;
+    } else {
+        return carCost * rentDays * discount;
+    }
+}
+console.log(rentalCarCost());
+
+function totalVacationCost() {
+
+    let hotel = hotelCost();
+
+    let planeRride = planeRideCost();
+
+    let car = rentalCarCost();
+
+    let totalCost = hotel + planeRride + car;
+
+    console.log(`Your hotel cost is: ${hotel}$, destination cost is: ${planeRride}$ and car rental cost is: ${car}$`);
+    console.log(`Total cost is: ${totalCost}$`);
+}
+
+totalVacationCost();
