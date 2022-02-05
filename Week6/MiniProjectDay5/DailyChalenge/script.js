6fae45bee3f5643637971bd95a57f9aa42c8f50a
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-02-05 23:08:52 +0200

    Daily Chalenge Day 5

--- Week6/MiniProjectDay5/DailyChalenge/script.js
+++ Week6/MiniProjectDay5/DailyChalenge/script.js
@@ -8,34 +8,14 @@
 
 // allTruthy(5, 4, 3, 2, 1, 0) ➞ false
 
-function allTruthy(a, b, c) {
-    let truthy = 0;
-    let falsey = 0;
+let arr = [120, 80, 7, 220, 77];
+let arr2 = [152, 344, 198, 139, 107];
 
-    if (!!a) {
-        truthy++;
-    } else { 
-        falsey++;
-    }
-    if (!!b) {
-         truthy++;
-    } else { 
-         falsey++;
-    }
-    if (!!c) {
-        truthy++;
-    } else { 
-        falsey++;
-    }
-    if (falsey > truthy) {
-        output = true;
-    }
-    if (falsey > 2) {
-        output = false;
-    }
-    return output;
-}
-console.log(allTruthy(true, null, undefined));
-console.log(allTruthy(false, '', '0'));
-console.log(allTruthy(false, 0, '0'));
- 
+function isLess(element, index) {
+    return element >= 100;
+  }
+    arr.every(isLess);  
+    arr2.every(isLess); 
+
+  console.log(arr.every(isLess))
+  console.log(arr2.every(isLess))
