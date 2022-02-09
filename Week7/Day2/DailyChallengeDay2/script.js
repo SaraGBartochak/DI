ffdf293006ee7de02db8c7cd6854889569993034
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-02-09 15:55:44 +0200

    Daily Chalenge Day 2

--- Week7/Day2/DailyChallengeDay2/script.js
+++ Week7/Day2/DailyChallengeDay2/script.js
@@ -7,13 +7,20 @@
 // Hint : Find an array method that returns the value of the first element in an array that pass a test.
 // Use the cars inventory below:
 
-let inventory = [
-  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-  { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
-  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-];
+  let inventory = [
+    { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+    { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+    { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+    { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+    { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  ];
+
+
+ let getCarHonda = carInventory => {
+    let carHonda = carInventory.find(e=>e.car_make === "Honda");
+    return `This is a ${carHonda.car_make} ${carHonda.car_model} from ${carHonda.car_year}`;
+   }
+  console.log(getCarHonda(inventory));
 
 
 // Part II
@@ -31,8 +38,17 @@ let inventory = [
   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
 ];
 
+  let sortCarInventoryByYear = carInventory => {
 
-// The output should be
+  let clonedArr =  [...carInventory];
+
+  return clonedArr.sort((car1, car2)=>car1.car_year - car2.car_year);
+};
+
+console.log(sortCarInventoryByYear(inventory)); 
+
+
+
 
 // [
 //   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
