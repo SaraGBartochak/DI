dc65c20ce0dca0daa738c6dc81444264baea0d25
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-03-14 22:08:56 +0200

    XP

---
+++ Week12/Day2/XPMandatory/npm-node/main.js
@@ -0,0 +1,28 @@
+// // Exercise 1 : Hello Node.Js
+// let a = 5;
+// let b = 10;
+
+// let sum = a+b;
+
+// console.log(`Hello you are ${sum}`);
+
+// Exercise 2 : Hello Node.Js #2
+
+console.log("Thanks for running me with npm start");
+
+// Exercise 3 : Nodemon
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+console.log(x + c);
+
+// let g = 35;
+// let f = 44;
+
+// let addition = g + f;
+
+// console.log( addition);
