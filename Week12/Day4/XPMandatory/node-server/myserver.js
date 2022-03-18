348cf1f407b52ba0f0c52cddc2ece5a0e7c54bdf
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-03-18 18:39:48 +0200

    XP Day4

---
+++ Week12/Day4/XPMandatory/node-server/myserver.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    console.log('headers', req.headers);
+    res.writeHead(200);
+    res.end(`<h1>This is HTML</h1>`+
+    '<h2>This is a new line</h2>'+
+    '<p>This is the last line</p>')
+})
+
+server.listen(3000, ()=>{
+    console.log('Server listening to port 3000')
+})
