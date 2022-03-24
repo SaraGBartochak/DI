4302110d01a20b9f84cb31f359d5fc961caf39da
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-03-24 20:14:18 +0200

    XP

--- Week13/Day2/fs.js
+++ Week13/Day2/fs.js
@@ -1,44 +1,25 @@
-const express = require('express')
-const app = express();
-
 const fs = require('fs');
-const dotenv = require('dotenv');
-dotenv.config();    
 
-app.use(express.json());
-app.use('/', express.static(__dirname + 'public'));
-app.listen(process.env.PORT||5500, () => {
-    console.log(`listen to port ${process.env.PORT}`)
-});
-app.get('/phrase', (req, res) => {
+fs.writeFile(`/phrase.txt`, 'Bla bla', err => {
 
-    const buff = fs.readFileSync(`${process.env.FILETOREAD}`); 
-    const jsonData = buff.toString();
+    if(err){
 
-    fs.writeFile(`/phrase.txt`, 'Some text', err => {
-
-        if(err){
-
-            res.json({msg: `ERROR : FILE ${process.env.FILETOWRITE}`});
-        }
-    })
-    
-    fs.appendFile(`/phrase.txt`, ' Buy orange juice', err => {
-
-        if(err){
-
-            res.json({msg: `ERROR : FILE ${process.env.FILETOWRITE}`});
-        }
-    })
-    
-
-    fs.unlink(`${process.env.FILETOAPPEND}`, err => {
-
-        if(err){
-
-            res.json({msg: `DELETING ERROR : FILE ${process.env.FILETOAPPEND}`});
-        }
-    })
-
-    res.json(jsonData); 
+        console.log("Error");
+    }
 })
+
+fs.appendFile(`/phrase.txt`, ' Buy orange juice', err => {
+
+    if(err){
+
+        console.log("Error");
+    }
+})
+
+fs.unlink(`/phrase.txt`, err => {
+
+    if(err){
+
+        console.log("Error");
+    }
+});
--- Week13/Day2/public/script.js
+++
@@ -1,6 +0,0 @@
-let loadPage = () => {
-    fetch(`/phrase`)
-    .then(res=> res.json())
-    .then(data=>console.log(data))
-    .catch(e=> console.log(e))
-}
