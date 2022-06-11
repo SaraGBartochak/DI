a6a989d75469094f42f21139aaa839d314dac5ab
SaraGBartochak <92598306+SaraGBartochak@users.noreply.github.com>
2022-06-11 17:17:32 +0300

    update

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -7,12 +7,13 @@ const  fileUpload = require('express-fileupload');
 
 const app = express();
 const port = process.env.PORT ||8000
-
+// parse application
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(fileUpload());
 
 app.use(cors());
+// parse application/json
 app.use(express.json());
 
 // DB
@@ -20,6 +21,7 @@ app.use(express.json());
 const knex = require('knex')({
     client:'pg',
     connection:{
+        filename: "./img.db",
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
@@ -27,33 +29,20 @@ const knex = require('knex')({
         database: process.env.DB_DATABASE
     }
 })
-app.get('/', async (req,res)=> {
-    res.send(`<form action="/upload" enctype="multipart/form-data" method="post">
-    <input type="file" name="pic" />
-    <input type="submit" value="Upload a file" />
-    </form>`);
+
+app.get('/', async (req, res) => {
+    res.send('Hello')
 })
 
-app.post('/upload', async (req, res) => {
-    const {name, data} = req.files.pic;
+app.post('/upload', async(req, res) => {
+    const {name, data} = req.files.img;
     if(name && data) {
         await knex.insert({name: name, img: data}).into('img');
         res.sendStatus(200);
     } else {
-        res.sendStatus(400); 
+        res.sendStatus(400);
     }
-});
-
-app.get('/img/:id', async (req, res) => {
-    const id = req.params.id;
-    const img = await knex('img').where({id: id}).first();
-    if (img) {
-        const contentType = await FileType.fromBuffer(img.img); // get the mimetype of the buffer (in this case its gonna be jpg but can be png or w/e)
-        res.end(img.img);
-    } else {
-        res.end('No Img with that Id!');
-    }
-});
+})
 
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`)
--- FinalProject/myautism-cards/src/App.js
+++ FinalProject/myautism-cards/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
       </header>
     </div>
   );
